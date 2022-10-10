a786a28fe1f2228a8383ac3c3ae0decbee64ce03
adastar <37849591+adastar@users.noreply.github.com>
2022-10-10 16:28:08 -0400

    Add files via upload

---
+++ MLT_AST_005.m
@@ -0,0 +1,60 @@
+classdef MLT_AST_005 < matlab.unittest.TestCase
+    % Copyright 2022 The MathWorks, Inc.
+    %
+    % TEST CASE DESCRIPTION:
+    % Test IsEqualTo constraint with verifyThat and assertThat unit test qualifications on
+    % all supported data types. (double, single, int8, int16, int32, int64,
+    % uint8, uint16, uint32, uint64, enumeration)
+    %
+    % EXPECTED RESULTS:
+    % Test steps 1-10 PASS 4 = 4
+    % Test step  11 PASS "abc" = "abc"
+    % Test step  12 FAIL "def" = "ded"
+    % Test step  13 FAIL 4 = 4.4
+    % Test step  14 FAIL 4.5 = 5
+    % Test steps 15-24 PASS 4 = 4
+    % Test step  25 PASS "abc" = "abc"
+    % Test step  26 FAIL "def" = "ded"
+    % Test step  27 FAIL 4 = 4.4
+    % Test step  28 FAIL 4.5 = 5
+    % Test steps 29-30 PASS Bearing.South = Bearing.Southwest
+    %     with Bearing is a enumeration class, South (180), Southwest (180)
+    %
+    % SUPPORTING FILES:
+    % None
+    properties (TestParameter)
+        baselineVal = struct('double', 4, 'single', single(4), 'int8', int8(4), ...
+            'uint8', uint8(4), 'int16', int16(4), 'uint16', uint16(4), ...
+            'int32', int32(4), 'uint32', uint32(4), 'int64', int64(4), ...
+            'uint64', uint64(4), 'stringPos', "abc", 'stringNeg','def','ts11', 4.4, 'ts12', 5);
+        actualVal = struct('double', 4, 'single', single(4), 'int8', int8(4), ...
+            'uint8', uint8(4), 'int16', int16(4), 'uint16', uint16(4), ...
+            'int32', int32(4), 'uint32', uint32(4), 'int64', int64(4), ...
+            'uint64', uint64(4), 'stringPos',"abc", 'stringNeg',"ded",'ts11', 4, 'ts12', 4.5);        
+    end
+    
+
+    methods (Test, ParameterCombination = 'sequential')
+
+        function testVerifyEqualTo(testCase, actualVal, baselineVal)
+            import matlab.unittest.constraints.IsEqualTo;
+            testCase.verifyThat(actualVal, IsEqualTo(baselineVal));
+        end
+
+        function testAssertEqualTo(testCase, actualVal, baselineVal)
+            import matlab.unittest.constraints.IsEqualTo;
+            testCase.assertThat(actualVal, IsEqualTo(baselineVal));
+        end
+
+        function testEnumVerifyEqualTo(testCase)
+            import matlab.unittest.constraints.IsEqualTo;
+            testCase.verifyThat(Bearing.South, IsEqualTo(Bearing.Southwest));
+        end
+
+        function testEnumAssertEqualTo(testCase)
+            import matlab.unittest.constraints.IsEqualTo;
+            testCase.assertThat(Bearing.South, IsEqualTo(Bearing.Southwest));
+        end
+    end
+
+end
